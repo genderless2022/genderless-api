06e8e9f7a0f4858a791ba0c1a8a3fd21d6545cbe
Romano Javier Alexis <alexis_b13@live.com.ar>
2022-04-26 15:13:27 -0300

    feat: ruta de idproducts

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,9 @@ router.get('/', (req, res)=>{
     res.send('hola mundo')
 })
 
-
+router.get('/idproducto', (req, res)=>{
+    res.send('aqui va la id de los productos')
+})
 
 
 module.exports = router;
