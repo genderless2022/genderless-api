18a24c920ffa9daf1924138d5b01ac03289680fa
Guillermo Jofre <jofregf@gmail.com>
2022-04-26 18:42:05 -0300

    Crear Usuario
    Se creo las funciones de crear usuario, generar token de sesión y encriptacion de password

---
+++ src/Middleware/generateToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const tokenSign = async (user) => {
+    try {
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                permission: user.permission,
+            },
+            process.env.KEY_TOKEN,
+            {
+                expiresIn: '1h',
+            },
+        )
+        return token;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {tokenSign};
