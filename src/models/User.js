2e8ee2ea973b534f673dfcb395de8a31982878c6
RoxanaCanciani <roxicanciani@gmail.com>
2022-04-26 17:22:22 -0300

    modelo Producto

--- src/models/User.js
+++ src/models/User.js
@@ -17,10 +17,6 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING
         },
 
-        gender: {
-            type: DataTypes.STRING,
-        },
-
         born: {
             type: DataTypes.DATE,
         },
