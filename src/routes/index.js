f27af9c285cff8cd8713479d55580494cab8aad2
Guillermo Jofre <jofregf@gmail.com>
2022-04-27 11:55:12 -0300

    User
    Funciones de crear usuario, hash de password, generación de token, obtener usuarios y las rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,14 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 
+
+
+
+
+
+
+const postUser = require('./user');
+router.use('/usuario', postUser);
+const getUsers = require('./user');
+router.use('/usuarios', getUsers);
 module.exports = router;
