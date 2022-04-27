8ff8e868a4d7573d14f2b88bd076d34287268852
Guillermo Jofre <jofregf@gmail.com>
2022-04-27 11:59:29 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,15 +8,10 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-
-
-
-
-
-
-
 const postUser = require('./user');
 router.use('/usuario', postUser);
 const getUsers = require('./user');
 router.use('/usuarios', getUsers);
+
+
 module.exports = router;
