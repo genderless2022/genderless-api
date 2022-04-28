cefb41c97dd8283503a82ed79a035023891d5f18
RoxanaCanciani <roxicanciani@gmail.com>
2022-04-27 22:04:42 -0300

    precarga y ruta del getProduct

--- src/Controllers/ProductControllers/getProduct.js
+++ src/Controllers/ProductControllers/getProduct.js
@@ -1,23 +1,24 @@
+
 const { Product, Category } = require('../../db.js');
 
-const getProduct = async (producto) => {
-    try {
-        const productCreated = await Product.create(producto);
-        try {
-            await Category.findOrCreate({
-                    where: { name: producto?.category }
-                });
-            }
-            catch (err) {
-                console.log(err);
-            }
 
-        const categoryCreated = await Category.findOne({ where: { name: producto?.category } });
-        await categoryCreated.addProduct(productCreated);
-    }
-    catch (error) {
+
+const getProduct = async(req,res)=>{
+
+    try {
+        
+        let allProducts = await Product.findAll({include:[
+            {model: Category}
+        ]})
+        
+      
+        res.send(allProducts)
+    
+        
+    } catch (error) {
         console.log(error);
     }
-};
-
-module.exports = getProduct;
+    
+    
+}
+    module.exports=getProduct;
--- src/preloader.js
+++ src/preloader.js
@@ -1,9 +1,9 @@
 const data = require('./productList');
-const getProduct = require('./Controllers/ProductControllers/getProduct')
+const preloaderProduct = require('./Controllers/ProductControllers/preloaderProduct');
 
 module.exports = async function preloader() {
 
-    data.map((p) => getProduct(p));
+    data.map((p) => preloaderProduct(p));
     console.log('Productos Pre-Cargados');
 
 };
