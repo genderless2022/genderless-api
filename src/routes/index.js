2d8fd4e2663c84768e369d3b47b10780d8c0f7cd
Romano Javier Alexis <alexis_b13@live.com.ar>
2022-05-02 09:13:07 -0300

    feat: ruta checkout

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,69 @@ const { Router } = require("express");
 
 const router = Router();
 
+const mercadopago = require('mercadopago');
+
+// Agrega credenciales
+mercadopago.configure({
+    access_token: "TEST-2503076328251445-050203-c66d4a54bede70270b5eca5596dec5b3-1116297917",
+})
+
+router.post("/checkout", (req, res) => {
+    /* lo que requiere por body
+    {
+        "name": ["pantalon", "jeans", "medias"],
+        "picture_url": ["asdsa", "asdads", "asdasda"],
+        "price": [23, 43, 12],
+        "quantity": [1, 3, 3]
+    } 
+    */
+    const data = req.body;
+    /* console.log(data); */
+    /* crea un objeto de preferencia */
+    let preference = {
+        items: [],
+        back_urls: {
+            success: "http://localhost:3000/success",
+            failure: "http://localhost:3000",
+            pending: "http://localhost:3000",
+        },
+        auto_return: "approved",
+        statement_descriptor: "GENDERLESS",
+        shipments: {
+            cost: 0,
+            mode: "not_specified",
+        },
+
+    };
+
+    if (Array.isArray(data.name)) {
+        for (let i = 0; i < data.name.length; i++) {
+            preference.items.push({
+                name: data.name[i],
+                picture_url: data.picture_url[i],
+                unit_price: parseInt(data.price[i]),
+                quantity: parseInt(data.quantity[i]),
+            });
+        }
+    } else {
+        preference.items.push({
+            name: data.name,
+            picture_url: data.picture_url,
+            unit_price: parseInt(data.price),
+            quantity: parseInt(data.quantity),
+        });
+    }
+    mercadopago.preferences
+        .create(preference)
+        .then(function (response) {
+            res.redirect(response.response.init_point);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+})
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 const getProduct = require('./product.js');
